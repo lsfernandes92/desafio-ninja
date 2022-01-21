cd577dbf397221c102d56a607b11dc0781b134cb
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-21 14:08:08 -0300

    Render JSON error message
    The behavior when I tried to retrieve an invalid user, e.g., making an 
    request for `GET /users/:id` or `DELETE /users/:id`, was an response 
    body with the rails server output exception.
    
    So, this edit substitute that rails server output exception with JSON 
    response.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_not_found_response(exception)
+    render json: { error_message: exception.message, error_status: '404' }, status: :not_found
+  end
 end
