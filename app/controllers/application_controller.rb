b1d7e7ed97b8091626355fe73c9ae2068e5fbcd4
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-24 12:40:50 -0300

    Implement content negociation mime type
    Following the jsonapi specification this changes aim to apply some 
    validation on `Accept` and `Content-Type` request headers.
    
    In resume, the specification tells that:
    - The client MUST send all requests with the header `Accept: 
    application/vnd.api+json` and ignore any response not in this format
    - The client MUST send all requests with data body with the header 
    `Content-Type: application/vnd.api+json`
    - The server MUST respond with the header `Content-Type: 
    application/vnd.api+json`
    - The server MUST respond "415 Unsupported Media Type" if `Content-Type` 
    header value different than `application/vnd.api+json`
    - The server MUST respond `406 Not Acceptable` if `Accept` header value 
    different than `application/vnd.api+json`
    
    Link reference from jsonapi: 
    https://jsonapi.org/format/#content-negotiation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,32 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  before_action :ensure_client_responsability
+  before_action :ensure_server_responsability
+
   def render_not_found_response(exception)
     render json: { error_message: exception.message, error_status: '404' }, status: :not_found
   end
+
+  def ensure_client_responsability
+    return if accept?
+    render nothing: true, status: :not_acceptable
+  end
+
+  def ensure_server_responsability
+    unless request.get?
+      return if content_type?
+      render nothing: true, status: :unsupported_media_type
+    end
+  end
+
+  private
+
+    def accept?
+      request.headers['Accept'] == 'application/vnd.api+json'
+    end
+
+    def content_type?
+      request.headers["Content-Type"] == 'application/vnd.api+json'
+    end
 end
