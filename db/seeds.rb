e9825320785197ad6a395080c3e57b0861d7fc3e
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-25 19:28:09 -0300

    Update seeds with Appointment data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,15 @@ puts '=== Creating some awesome Users...'
   )
 end
 puts '=== Users successfully created!'
+
+puts '=== Creating some Appointments...'
+5.times do |_i|
+  Appointment.create!(
+    title: Faker::Lorem.sentence(word_count: 3),
+    notes: Faker::Lorem.sentence(word_count: 5),
+    start_time: Faker::Time.forward(days: 1, period: :morning),
+    end_time: Faker::Time.forward(days: 1, period: :evening),
+    user: User.all.sample
+  )
+end
+puts '=== Appointments successfully created!'
