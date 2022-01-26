44e134bba0d39f9728b059eca884de99a081047d
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-26 15:48:57 -0300

    Remove `pt-BR` default locale
    I thought that setting default locale to `pt-BR` was needed to localize 
    the output in the attributes `start_time` and `end_time` from 
    `Appointment` model, but instead I did this by overwriting the 
    `attributes` 
    method from the appointment serializer and creating a new time format 
    locale.

--- app/serializers/v1/appointment_serializer.rb
+++ app/serializers/v1/appointment_serializer.rb
@@ -3,5 +3,12 @@ module V1
     attributes :id, :title, :notes, :start_time, :end_time
 
     belongs_to :user
+
+    def attributes(*args)
+      h = super(*args)
+      h[:start_time] = (I18n.l(object.start_time) unless object.start_time.blank?)
+      h[:end_time] = (I18n.l(object.end_time) unless object.end_time.blank?)
+      h
+    end
   end
 end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,2 +1,1 @@
 # Set default locale to something other the :en
-I18n.default_locale = 'pt-BR'
