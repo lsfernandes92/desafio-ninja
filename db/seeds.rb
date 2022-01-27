3d6c100fd0df9686bce1eaa6f839047b24ea999a
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-26 23:06:25 -0300

    Update seeds with some appointments

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,39 @@ end
 puts '=== Users successfully created!'
 
 puts '=== Creating some Appointments...'
-5.times do |_i|
-  Appointment.create!(
-    title: Faker::Lorem.sentence(word_count: 3),
-    notes: Faker::Lorem.sentence(word_count: 5),
-    start_time: Faker::Time.forward(days: 1, period: :morning),
-    end_time: Faker::Time.forward(days: 1, period: :evening),
-    user: User.all.sample
-  )
-end
+Appointment.create!(
+  title: Faker::Lorem.sentence(word_count: 3),
+  notes: Faker::Lorem.sentence(word_count: 5),
+  start_time: Time.zone.local(2022, 12, 26, 9, 0, 0),
+  end_time: Time.zone.local(2022, 12, 26, 18, 0, 0),
+  user: User.all.sample
+)
+Appointment.create!(
+  title: Faker::Lorem.sentence(word_count: 3),
+  notes: Faker::Lorem.sentence(word_count: 5),
+  start_time: Time.zone.local(2022, 12, 27, 9, 0, 0),
+  end_time: Time.zone.local(2022, 12, 27, 18, 0, 0),
+  user: User.all.sample
+)
+Appointment.create!(
+  title: Faker::Lorem.sentence(word_count: 3),
+  notes: Faker::Lorem.sentence(word_count: 5),
+  start_time: Time.zone.local(2022, 12, 29, 9, 0, 0),
+  end_time: Time.zone.local(2022, 12, 29, 12, 0, 0),
+  user: User.all.sample
+)
+Appointment.create!(
+  title: Faker::Lorem.sentence(word_count: 3),
+  notes: Faker::Lorem.sentence(word_count: 5),
+  start_time: Time.zone.local(2022, 12, 29, 12, 1, 0),
+  end_time: Time.zone.local(2022, 12, 29, 13, 0, 0),
+  user: User.all.sample
+)
+Appointment.create!(
+  title: Faker::Lorem.sentence(word_count: 3),
+  notes: Faker::Lorem.sentence(word_count: 5),
+  start_time: Time.zone.local(2022, 12, 29, 17, 1, 0),
+  end_time: Time.zone.local(2022, 12, 29, 18, 0, 0),
+  user: User.all.sample
+)
 puts '=== Appointments successfully created!'
