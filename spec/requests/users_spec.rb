e5f36dbd6c9b91594403c2e99b227639ad01a8cc
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-27 10:01:31 -0300

    Add content-type header check test to user reasource

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,10 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Users requests', type: :request do
+  let(:accept_header) { { "Accept": 'application/vnd.api+json' } }
+  let(:content_type_header) { { "Content-Type": 'application/vnd.api+json' } }
+
   context 'when request with invalid headers' do
     it 'returns status code 406 if no accept header sent' do
       get '/v1/users'
       expect(response).to have_http_status(:not_acceptable)
+    end
+
+    it 'returns status code 415 if no content-type header sent' do
+      post('/v1/users', headers: accept_header)
+      expect(response).to have_http_status(:unsupported_media_type)
     end
   end
 
@@ -14,9 +22,6 @@ RSpec.describe 'Users requests', type: :request do
     before do
       create_list(:user, 2)
     end
-
-    let(:accept_header) { { "Accept": 'application/vnd.api+json' } }
-    let(:content_type_header) { { "Content-Type": 'application/vnd.api+json' } }
 
     describe 'GET /users' do
       before do
