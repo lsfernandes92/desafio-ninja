5241798bd6903564424f98b1e3bf0c9bbc264b5a
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-26 23:07:02 -0300

    Include helpers that help you test passage of time in tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,7 +36,11 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # FactoryBot needed methods
   config.include FactoryBot::Syntax::Methods
+
+  # Contains helpers that help you test passage of time in tests
+  config.include ActiveSupport::Testing::TimeHelpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
