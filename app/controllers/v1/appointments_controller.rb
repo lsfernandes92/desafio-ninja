59c2a371f0e8e4e70142a9dee7d915a9a970f41e
lsfernandes92 <lsfernandes92@gmail.com>
2022-01-28 14:50:34 -0300

    Update appointment show method to respond with resource appointments

--- app/controllers/v1/appointments_controller.rb
+++ app/controllers/v1/appointments_controller.rb
@@ -4,12 +4,14 @@ module V1
   class AppointmentsController < ApplicationController
     include ErrorSerializer
 
-    before_action :set_user
+    before_action :set_user, only: %i[create update destroy]
+    before_action :set_resource, only: :show
 
     def show
       page_number = params[:page].try(:[], :number)
       per_page = params[:page].try(:[], :size)
-      appointments = @user.appointments.all.page(page_number).per(per_page)
+
+      appointments = @resource.appointments.all.page(page_number).per(per_page)
 
       render json: appointments
     end
@@ -43,6 +45,15 @@ module V1
 
     def set_user
       @user = User.find(params[:user_id])
+    end
+
+    def set_resource
+      @resource =
+        if params[:room_id].present?
+          Room.find(params[:room_id])
+        else
+          User.find(params[:user_id])
+        end
     end
 
     def appointment_params
